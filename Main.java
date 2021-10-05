6a0ce85177bdb8cdb68fe8eef4b6cd4e0af5d34a
winchjr <winchesterjoshuar@gmail.com>
2021-10-05 13:41:05 -0700

    connected database, for loop w/ scraping

--- Main.java
+++ Main.java
@@ -1,37 +1,28 @@
 package flightScraper;
-import java.io.FileWriter;
+
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+
 
 //when called, scrapes mccarran airport arrivals and exports data dump to postgresql for storage.
 
 class Main {
+		
+	
 	public static void main (String[] args) throws InterruptedException, IOException {
 		
+		final int totalScrapes = 5;
 		//make a new scraper
 		Scraper scraper = new Scraper();		
 				
-		//gets total flights by telling the scraper to scrape
-		int flightTotal = scraper.scrape();
+		//scrape five times
+		for (int i = 0; i<totalScrapes; i++) {
+		//tells the scraper to scrape
+		scraper.scrape();				
+	
+		}
+		//publish the flight totals, date, and ampm to the db
+		scraper.publish();
 		
-		//get local datetime and format
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime now = LocalDateTime.now();  
-		String submissionDate = dtf.format(now);
-		String output = submissionDate + ": " + flightTotal;
-		System.out.println(output);
-		
-		//store datetime and flighttotals to a text file
-		FileWriter fw = new FileWriter("/path/to/flightdata.txt", true);
-		fw.write(output + "\n");
-		
-		fw.close();
-		//send the data to the database
-		
-		//calls python code for data analysis when complete
 	}
 	
 	
